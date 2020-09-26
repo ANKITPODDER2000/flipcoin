bb3f9eb7d6dae20916239c773f62c605fef42108
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-26 18:11:43 +0530

    Updates

---
+++ precache-manifest.36fd4eb4396871cd246097087fa3c892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4398e5d0c6b6db92b92d8d92dd891793",
+    "url": "/flipcoin/index.html"
+  },
+  {
+    "revision": "74f430961c447c541ec1",
+    "url": "/flipcoin/static/css/main.8c2273d7.chunk.css"
+  },
+  {
+    "revision": "2fc60b7f6167628f1583",
+    "url": "/flipcoin/static/js/2.26e43191.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flipcoin/static/js/2.26e43191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f430961c447c541ec1",
+    "url": "/flipcoin/static/js/main.1038d52d.chunk.js"
+  },
+  {
+    "revision": "c1de7cd3bbd8bed25ac5",
+    "url": "/flipcoin/static/js/runtime-main.c3959667.js"
+  }
+]);
